1939038a3e2f5affc07430df3469ddc4e3c29e81
ducksteam <106374514+ducksteam33@users.noreply.github.com>
2022-12-05 20:43:21 +0100

    Add files via upload

---
+++ setup.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+mkdir /opt/wci \
+	  /opt/wci/main  \
+	  /opt/wci/cache  \
+	  /opt/wci/main/captures \ 
+	  /srv/http/wciPage 2>/dev/null
+
+
+rm /etc/httpd/conf/httpd.conf
+rm /etc/httpd/conf/extra/httpd-vhosts.conf
+cp ./source/confFiles/httpd.conf /etc/httpd/conf/
+cp ./source/confFiles/httpd-vhosts.conf /etc/httpd/conf/extra
+cp -r ./source  /opt/wci/main
+cp -r ./source/confFiles/templates  /opt/wci/main/source
+
+
+pacman -S xterm aircrack-ng hostapd apache python python-pip dnsmasq --noconfirm #&>/dev/null
+pip install click
+
+clear
+
+echo Done! Now you just can cd into 'source' and type 'wci -h' to get started!
+
+
